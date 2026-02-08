d42e1ca775b3422de51d2785c603397a081c2b07
Mohamed Fakroun <mohfakroun@gmail.com>
2026-02-08 20:09:11 +0000

    fix: favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,7 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+       <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body className="bg-white text-zinc-950">
         <Main />
